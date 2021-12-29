685d7c8036812898b3fc643e9e46dbf34b4f4714
ChayaF <ch.f5948@gmail.com>
2021-12-29 18:17:58 +0200

    In additon to the last add

--- Assets/Scripts/GameManager/UiManager.cs
+++ Assets/Scripts/GameManager/UiManager.cs
@@ -18,9 +18,13 @@ public class UiManager : MonoBehaviour
         gameManager.OnFlagConquered += ChangeTheNumberFlagsText;
     }
 
-    private void ChangeTheNumberFlagsText(int flagsP1,int flagsP2)
+    private void ChangeTheNumberFlagsText(int flagsP1,int flagsP2,bool conquered)
     {
-        flagTextP1.text = $"flags:{flagsP1.ToString()}";
-        flagTextP2.text = $"flags:{flagsP2.ToString()}";
+        if (conquered)
+        {
+            flagTextP1.text = $"flags:{flagsP1.ToString()}";
+            flagTextP2.text = $"flags:{flagsP2.ToString()}";
+        }
+       
     }
 }
