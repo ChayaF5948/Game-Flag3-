7b14984e3be10807b5010302827dd286f288e2e6
ChayaF <ch.f5948@gmail.com>
2022-01-08 22:56:22 +0200

    Add text of the amount of bullets  player has

--- Assets/Scripts/GameManager/UiManager.cs
+++ Assets/Scripts/GameManager/UiManager.cs
@@ -1,21 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 using UnityEngine.UI;
 
 public class UiManager : MonoBehaviour
 {
+    [SerializeField]
     private GameManager gameManager;
-    
+    [SerializeField]
+    private PlayerHPdata[] playerData;
+
     [SerializeField]
     private Text flagTextP1;
     [SerializeField]
     private Text flagTextP2;
 
+    [SerializeField]
+    private Text bulletTextP1;
+    [SerializeField]
+    private Text bulletTextP2;
+
+
     private void Start()
     {
-        gameManager = GameObject.FindObjectOfType<GameManager>();
         gameManager.OnFlagConquered += ChangeTheNumberFlagsText;
+        playerData[0].OnAddOrDecreaseBullet += ChangeTheNumberBulletsP1Text;
+        playerData[1].OnAddOrDecreaseBullet += ChangeTheNumberBulletsP2Text;
     }
 
     private void ChangeTheNumberFlagsText(int flagsP1,int flagsP2,bool conquered)
@@ -24,7 +33,16 @@ public class UiManager : MonoBehaviour
         {
             flagTextP1.text = $"flags:{flagsP1.ToString()}";
             flagTextP2.text = $"flags:{flagsP2.ToString()}";
-        }
-       
+        }  
+    }
+
+    private void ChangeTheNumberBulletsP1Text(int bulletP1)
+    {
+        bulletTextP1.text = $"Bullet:{bulletP1.ToString()}";
+    }
+
+    private void ChangeTheNumberBulletsP2Text(int bulletP2)
+    {
+        bulletTextP2.text = $"Bullet:{bulletP2.ToString()}";
     }
 }
